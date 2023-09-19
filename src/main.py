b234c71e135ed52cb5178b036614a4f598e4def3
Illyrius <FitimQ@live.nl>
2023-09-19 15:40:12 +0200

    -

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-class Main():
-    def __init__(self, APIKEY=''):
+class Main:
+    """Main class"""
+
+    def __init__(self, APIKEY=""):
+        """Constructor"""
         self.api_key = APIKEY
-        
+
     def test(self):
+        """Test function"""
         print(self.api_key)
