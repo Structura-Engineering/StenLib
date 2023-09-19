db60ee597e7d7df49c732a5745f664f23e86b4c2
Illyrius <FitimQ@live.nl>
2023-09-19 14:11:21 +0200

    -

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # QeLib
+
+# Upload to PyPi:
+
+1. Update version in setup.py
+2. Run `python setup.py sdist`
+3. Run `twine upload dist/*`
---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='QeLib',
+    version='0.0.1',
+    description='Qerimi Engineering Library',
+    author='Illyrius',
+    license='GNU AGPLv3',
+    packages=['src'],
+)
