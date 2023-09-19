2f92413d10af20874e98fab855feaf265e8a0519
Illyrius <FitimQ@live.nl>
2023-09-19 15:53:35 +0200

    0.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 st.setup(
     name="QeLib",
-    version="0.0.5",
+    version="0.0.6",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Illyrius",
