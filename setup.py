b2b453183d33fb7f5b244196dbe140608cb4383b
Illyrius <FitimQ@live.nl>
2023-09-19 15:56:29 +0200

    0.0.7

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 
 st.setup(
     name="QeLib",
-    version="0.0.6",
+    version="0.0.7",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Illyrius",
     license="LICENSE.md",
     packages=st.find_packages(),
     python_requires=">=3.11.5",
-    install_requires=["setuptools", "string", "random"],
+    install_requires="setuptools",
 )
