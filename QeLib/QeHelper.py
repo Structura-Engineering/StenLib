0b41a59fa35add2046c2fcaac84fd77c519753b1
Illyrius <FitimQ@live.nl>
2023-09-21 13:20:13 +0200

    -

--- QeLib/QeHelper.py
+++ QeLib/QeHelper.py
@@ -11,8 +11,33 @@ def complex_id_generator(char_len=6):
     return "".join(random.choice(chars) for _ in range(abs(char_len)))
 
 
-def center_ui_on_screen(ui):
+def center_ui_on_screen(ui) -> None:
     """see *.pyi file for docstring"""
     screen_center = QApplication.primaryScreen().geometry().center()
     ui_size = ui.size()
     ui.move(screen_center - QPoint(ui_size.width() // 2, ui_size.height() // 2))
+
+def set_ui_size(ui, size=None) -> None:
+    """see *.pyi file for docstring"""
+    if size is None:
+        size = ui.minimumSizeHint()
+    ui.resize(*size)    
+
+def toggle_ui_visibility(uis) -> None:
+    """Toggles the visibility of UI(s)."""
+    for ui in uis:
+        ui.setVisible(not ui.isVisible())
+        if ui.isVisible():
+            center_ui_on_screen(ui)
+
+def switch_modules(module) -> None:
+    """Switches the modules."""
+    current_index = module.currentIndex()
+    new_index = (current_index + 1) % module.count()
+    module.setCurrentIndex(new_index)
+
+def fit_scene_in_view(instance) -> None:
+    """Fits the scene in the view."""
+    instance.fitInView(
+        instance.sceneRect(), AspectRatioModeTypes.KeepAspectRatio.value
+    )
--- QeLib/QeHelper.pyi
+++ QeLib/QeHelper.pyi
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from PySide6.QtWidgets import QWidget
+
 def complex_id_generator(char_len: int) -> str:
     """
     Generate a complex ID.
@@ -18,4 +22,45 @@ def complex_id_generator(char_len: int) -> str:
         >>> complex_id_generator(8)
         '4FwS7eZq'
     """
+    ...
+    
+def center_ui_on_screen(ui: QWidget) -> None:
+    """
+    Center a UI on the screen.
+    
+    Parameters:
+        ui (QWidget): The UI to center on the screen.
+        
+    Returns:
+        None: This function does not return anything.
+        
+    This function centers a UI on the screen. It is useful for centering a UI on the
+    screen when it is first displayed.
+    
+    Example:
+        >>> center_ui_on_screen(ui)
+    """
+    ...
+    
+def set_ui_size(ui: QWidget, size: Optional[tuple[int, int]]=None) -> None:
+    """Set the size of a UI.
+
+    Parameters:
+        ui (QWidget): The UI to set the size of.
+        size (tuple, optional): The size to set the UI to. Default is None.
+
+    Returns:
+        None: This function does not return anything.
+
+    This function sets the size of a UI. If the `size` parameter is not provided, the
+    size of the UI is set to the size of the UI's minimum size hint.
+
+    Example:
+        >>> set_ui_size(ui)
+        >>> set_ui_size(ui, (800, 600))
+    """    
+    ...
+    
+def toggle_ui_visibility(uis: list[QWidget]) -> None:
+    
     ...
