ac8b7d0a9986bb7e135caa0ea5c5def45bdc34cd
Illyrius <FitimQ@live.nl>
2023-10-06 10:26:16 +0200

    .test .tag[0.0.26]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="QeLib",
-    version="0.0.25",
+    version="0.0.26",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=open("LICENSE.md").read(),
