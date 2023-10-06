92903d4a16e4e32e9edca0e8b2ccbc6e29f74e0e
Illyrius <FitimQ@live.nl>
2023-10-06 16:40:45 +0200

    .test .tag[0.0.35]

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="QeLib",
-    version="0.0.34",
+    version="0.0.35",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=open("LICENSE.md").read(),
-    url="https://github.com/Illyrius666/QeLib",
-    project_urls={"Bug Tracker": "https://github.com/Illyrius666/QeLib/issues"},
+    url="https://github.com/Illyrius666/Qengineering",
+    project_urls={"Bug Tracker": "https://github.com/Illyrius666/Qengineering/issues"},
     packages=find_packages(),
     python_requires=">=3.11.5",
     package_data={
