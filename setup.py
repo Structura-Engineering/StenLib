7af656b0456ed771dfc964835e2ee8d86936e8f7
Illyrius <FitimQ@live.nl>
2023-10-06 10:46:47 +0200

    .test .tag[0.0.28] removed install_requires[] # not needed anymore.

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="QeLib",
-    version="0.0.27",
+    version="0.0.28",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=open("LICENSE.md").read(),
     url="https://github.com/Illyrius666/QeLib",
     project_urls={"Bug Tracker": "https://github.com/Illyrius666/QeLib/issues"},
-    install_requires=["twine", "wheel", "pyside6", "shiboken6", "sphinx"],
     packages=find_packages(),
     python_requires=">=3.11.5",
     package_data={
