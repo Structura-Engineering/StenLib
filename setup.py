61b912a43e449f9c3247e8a8a866a7f32b47bacb
Illyrius <FitimQ@live.nl>
2023-10-12 09:18:08 +0200

    .test .tag[0.0.38]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="StenLib",
-    version="0.0.37",
+    version="0.0.38",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=open("LICENSE.md").read(),
--- src/StenLib/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-modules = [
-    f[:-3]
-    for f in os.listdir(os.path.dirname(__file__))
-    if f.endswith(".py") and f != "__init__.py"
-]
-
-__all__ = [m for m in modules if not m.startswith("_")]
