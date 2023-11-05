eddd10d3ee1269d4f76a4e4a3ceb5e3e9bba643c
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-11-05 15:04:37 +0100

    .deploy .tag[0.1.0]

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import find_packages, setup
 
-from StenLib.StenUtils import Utils
-
 setup(
     name="StenLib",
     version="0.1.0",
-    long_description=Utils.read_file("README.md"),
+    long_description="README.md",
     long_description_content_type="text/markdown",
-    license=Utils.read_file("LICENSE.md"),
+    license="LICENSE.md",
     url="https://github.com/Structura-Engineering/StenLib",
     project_urls={
         "Bug Tracker": "https://github.com/Structura-Engineering/StenLib/issues"
     },
-    install_requires=Utils.read_file("requirements.txt").splitlines(),
+    install_requires="requirements.txt",
     packages=find_packages(),
     python_requires=">=3.12.0",
     package_data={
