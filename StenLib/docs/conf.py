205ad6c23e48a3fcbbd542a8e4111dee5a1f4701
Illyrius <FitimQ@live.nl>
2023-12-06 15:16:23 +0000

    Update CSS files in StenLib documentation

--- StenLib/docs/conf.py
+++ StenLib/docs/conf.py
@@ -33,23 +33,16 @@ html_theme_options = {
     "analytics_id": "",
     "analytics_anonymize_ip": False,
     "logo_only": False,
-    "display_version": True,
-    "prev_next_buttons_location": "bottom",
+    "display_version": False,
+    "prev_next_buttons_location": "both",
     "style_external_links": True,
     "vcs_pageview_mode": "",
     "style_nav_header_background": "#FFB000",
     "collapse_navigation": True,
     "sticky_navigation": True,
-    "navigation_depth": 4,
-    "includehidden": True,
+    "navigation_depth": -1,
+    "includehidden": False,
     "titles_only": False,
 }
 html_static_path = ["_static"]
-# html_css_files = ["css/custom.css"] # TODO: add custom css.(https://docs.readthedocs.io/en/stable/guides/adding-custom-css.html)
-# TODO: Fix bug in `make html` giving:
-# WARNING: autodoc: failed to import module 'StenEnumUtils' from module 'StenLib'; the following exception was raised:
-# No module named 'StenLib'
-# WARNING: autodoc: failed to import module 'StenUtils' from module 'StenLib'; the following exception was raised:
-# No module named 'StenLib'
-# WARNING: autodoc: failed to import module 'StenLib'; the following exception was raised:
-# No module named 'StenLib'
+html_css_files = ["css/sten_theme.css"]
