f9fad1a3eb4db0a591e015f9fb73b064d31f6cc2
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-12-11 13:36:21 +0000

    .deploy .tag[0.3.0]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="StenLib",
-    version="0.2.9",
+    version="0.3.0",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=open("LICENSE.md").read(),
