af715a2c6c5689a8c756959cedabf197d2d534cb
illyrius666 <FitimQ@live.nl>
2023-12-14 10:59:35 +0000

    Update python_requires in setup.py to use exact version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     },
     install_requires=open("requirements.txt").read().splitlines(),
     packages=find_packages(),
-    python_requires="=3.12.0",
+    python_requires="==3.12.0",
     package_data={"*": ["*.py"]},
     zip_safe=False,
 )
