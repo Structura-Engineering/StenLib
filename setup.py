bea4685a87a2a5866111f20def003c40205c5ce3
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-12-19 15:20:17 +0000

    .deploy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='StenLib',
-    version='0.7.4',
+    version='0.7.5',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license=open('LICENSE.md').read(),
