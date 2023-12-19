9959c24104fb9d4c02a20097ff981327556f5c5f
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-12-19 15:27:27 +0000

    Add favicon to HTML output .deploy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='StenLib',
-    version='0.7.5',
+    version='0.7.6',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license=open('LICENSE.md').read(),
